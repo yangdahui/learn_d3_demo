b43553ec8a7974cada95c54e038f59836c3f51a1
yangdahui <yangdahui@hotmail.com>
2013-05-17 10:22:59 -0400

    +bullet chart add indicator and measure text

--- learn_d3_demo/public_html/js/bulletchart.js
+++ learn_d3_demo/public_html/js/bulletchart.js
@@ -3,9 +3,11 @@
  * and open the template in the editor.
  */
 
-var margin = {top: 5, right: 40, bottom: 20, left: 120},
-    width = 300 - margin.left - margin.right,
-    height = 40 - margin.top - margin.bottom;
+var margin = {top: 5, right: 60, bottom: 15, left: 60},
+    width = 400 - margin.left - margin.right,
+    height = 35 - margin.top - margin.bottom;
+var textposition = width +40;
+var indicatorposition = textposition + 15;
 
 var chart = d3.bullet()
     .width(width)
@@ -21,7 +23,8 @@ d3.json("data/bulletsdata.json", function(error, data) {
     .append("g")
       .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
       .call(chart);
-
+      
+//add title
   var title = svg.append("g")
       .style("text-anchor", "end")
       .attr("transform", "translate(-6," + height / 2 + ")");
@@ -30,20 +33,28 @@ d3.json("data/bulletsdata.json", function(error, data) {
       .attr("class", "title")
       .text(function(d) { return d.title; });
 
+ //add subtitle
   title.append("text")
       .attr("class", "subtitle")
       .attr("dy", "1em")
       .text(function(d) { return d.subtitle; });
       
+//present number at the end of each bullet      
   title.append("text")
         .attr("class","measurevalue")
-        .attr("transform", "translate(340," + height / 2 + ")")
+//        .attr("transform", "translate(340," + height / 2 + ")")
+        .attr("transform", "translate(" + textposition+ "," + height / 2 + ")")
         .text(function(d) { return d.measures; });
 
+//if measure is above the normal range, add a red indicator
+//TODO: if indicator then red dot
   title.append("circle")
         .attr("r",8)
         .style("fill","red")
-        .attr("transform", "translate(345," + height / 2 + ")");     
+//      .attr("transform", "translate(345," + height / 2 + ")");     
+        .attr("transform", "translate(" + indicatorposition + "," + 0 + ")");
+
+
 
   d3.selectAll("button").on("click", function() {
     svg.datum(randomize).call(chart.duration(1000)); // TODO automatic transition
